d64c25639383a04ef7a77727afeb07fcebc7fd76
TonyGlide <162181740+TonyGlide@users.noreply.github.com>
2024-04-02 00:04:29 +0000

    up_1

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello Tony");
+
+  let who = ["The dog", "my grandma", "The mailman", "The bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function selectArrayElement(arrayName, arrayLength) {
+    let randomNumber = Math.floor(Math.random() * arrayLength);
+    let selectedElement = arrayName[randomNumber];
+    return selectedElement;
+  }
+
+  let randomWho = selectArrayElement(who, who.length);
+  let randomAction = selectArrayElement(action, action.length);
+  let randomWhat = selectArrayElement(what, what.length);
+  let randomWhen = selectArrayElement(when, when.length);
+
+  let escuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+
+  console.log(escuse);
 };
